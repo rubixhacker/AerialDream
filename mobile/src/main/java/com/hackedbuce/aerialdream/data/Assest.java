ba94a5dcc35cb0f83e1c48ba2ede509d417e7d94
Stewart Boling <rubixhacker@gmail.com>
2016-01-10 16:02:57 -0600

    Initial commit

---
+++ mobile/src/main/java/com/hackedbuce/aerialdream/data/Assest.java
@@ -0,0 +1,27 @@
+package com.hackedbuce.aerialdream.data;
+
+/**
+ * Created by rubix on 1/10/2016.
+ */
+public class Assest {
+    String url;
+    String accessibilityLabel;
+    String id;
+    String timeOfDay;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getAccessibilityLabel() {
+        return accessibilityLabel;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTimeOfDay() {
+        return timeOfDay;
+    }
+}
---
+++ mobile/src/main/java/com/hackedbuce/aerialdream/data/Videos.java
@@ -0,0 +1,19 @@
+package com.hackedbuce.aerialdream.data;
+
+import java.util.List;
+
+/**
+ * Created by rubix on 1/10/2016.
+ */
+public class Videos {
+    String id;
+    List<Assest> assets;
+
+    public String getId() {
+        return id;
+    }
+
+    public List<Assest> getAssests() {
+        return assets;
+    }
+}
